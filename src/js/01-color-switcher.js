fa1734645935635faa9d213a76937f812d6cf2de
olehpetrovasyliev <olegvasiilyevv@gmail.com>
2023-05-29 17:24:46 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,2 +1,7 @@
 const startBtn = document.querySelector('[dataset.start]');
 const stopBtn = document.querySelector('[dataset.stop]');
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
