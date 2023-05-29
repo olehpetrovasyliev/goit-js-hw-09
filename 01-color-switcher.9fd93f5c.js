7766a26face01e910a2dd4c90dc7c8d54c6ef33b
olehpetrovasyliev <128256070+olehpetrovasyliev@users.noreply.github.com>
2023-05-29 14:25:10 +0000

    Deploying to gh-pages from @ olehpetrovasyliev/goit-js-hw-09@fa1734645935635faa9d213a76937f812d6cf2de 🚀

---
+++ 01-color-switcher.9fd93f5c.js
@@ -0,0 +1,2 @@
+document.querySelector("[dataset.start]"),document.querySelector("[dataset.stop]");
+//# sourceMappingURL=01-color-switcher.9fd93f5c.js.map
---
+++ 01-color-switcher.be38ae68.js
@@ -0,0 +1,2 @@
+document.querySelector("[dataset.start]"),document.querySelector("[dataset.stop]");
+//# sourceMappingURL=01-color-switcher.be38ae68.js.map
