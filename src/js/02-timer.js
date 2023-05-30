e7c5278f89dec5b13be757e3e38547999eb556b5
olehpetrovasyliev <olegvasiilyevv@gmail.com>
2023-05-30 15:39:07 +0300

    1

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,25 @@
+//imports
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+// finding elements in dom
 const picker = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('[data-start]');
 
-const now = new Date();
-const deadline = new Date(picker.value);
+// obj for flatpickr
 
-const diff = deadline - now;
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    // date variables;
+    const now = new Date();
+    const deadline = new Date(picker.value);
+    const diff = deadline - now;
+    console.log(diff);
+  },
+};
+
+flatpickr(picker, options);
