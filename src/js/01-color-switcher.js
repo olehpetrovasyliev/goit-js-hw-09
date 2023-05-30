1c928f27a7c723c32b579973ba64d7d45924fecf
olehpetrovasyliev <olegvasiilyevv@gmail.com>
2023-05-30 09:15:16 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,5 +1,25 @@
-const startBtn = document.querySelector('[dataset.start]');
-const stopBtn = document.querySelector('[dataset.stop]');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timerId = null;
+
+// body.style.backgroundColor = getRandomHexColor();
+
+const colorChanger = () => {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+  }, 1000);
+  //  startBtn.removeEventListener('click', colorChanger);
+};
+startBtn.addEventListener('click', colorChanger);
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
