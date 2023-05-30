3f71c05ddf7efbd565e166635e76085e9a81122c
olehpetrovasyliev <128256070+olehpetrovasyliev@users.noreply.github.com>
2023-05-30 12:39:30 +0000

    Deploying to gh-pages from @ olehpetrovasyliev/goit-js-hw-09@e7c5278f89dec5b13be757e3e38547999eb556b5 🚀

--- 02-timer.74acc8d2.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("#datetime-picker"),t=new Date;new Date(e.value);
-//# sourceMappingURL=02-timer.74acc8d2.js.map
--- 02-timer.c1a149f5.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("#datetime-picker"),t=new Date;new Date(e.value)}();
-//# sourceMappingURL=02-timer.c1a149f5.js.map
