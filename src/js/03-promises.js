bcd634b2e9883d877c8b7591291a61c5b5240b24
olehpetrovasyliev <olegvasiilyevv@gmail.com>
2023-05-31 14:44:19 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,10 +1,12 @@
-function createPromise(position, delay) {
+function createPromise(position, delay, firstDelay) {
   const shouldResolve = Math.random() > 0.3;
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {}, firstDelay);
+  });
+
   if (shouldResolve) {
     // Fulfill
   } else {
     // Reject
   }
 }
-
-const promise = new Promise((resolve, reject) => {});
