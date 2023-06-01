d4e87d1198589e81a020be32f886dd6db2e0ec5a
olehpetrovasyliev <olegvasiilyevv@gmail.com>
2023-06-01 13:51:15 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -15,51 +15,49 @@ function createPromise(position, delay) {
     }, delay);
   });
 }
+
 function onSubmit(evt) {
   evt.preventDefault();
   const delayInp = Number(evt.currentTarget.elements.delay.value);
   const amount = Number(evt.currentTarget.elements.amount.value);
   const step = Number(evt.currentTarget.elements.step.value);
-  let totalDelay = 0;
-  for (let i = 0; i < amount; i++) {
-    let position = i + 1;
-    // delay += position;
-    // console.log(i);
 
-    const timerId = setInterval(() => {
-      createPromise(position, step)
+  for (let i = 1; i <= amount; i++) {
+    let position = i;
+    let currentDelay = i === 1 ? delayInp : delayInp + step * (position - 1);
+
+    setTimeout(() => {
+      createPromise(position, currentDelay)
         .then(({ position, delay }) => {
           if (position === 1) {
             Notiflix.Notify.success(
-              `&#9989 Fulfilled promise ${position} in ${delayInp}ms`
+              `&#9989 Fulfilled promise ${position} in ${currentDelay}ms`
             );
           } else {
             Notiflix.Notify.success(
-              `&#9989 Fulfilled promise ${position} in ${
-                delayInp + step * (position - 1)
-              }ms`
+              `&#9989 Fulfilled promise ${position} in ${currentDelay}ms`
             );
           }
         })
         .catch(({ position, delay }) => {
           if (position === 1) {
             Notiflix.Notify.failure(
-              `&#10060 Rejected promise ${position} in ${delayInp}ms`
+              `&#10060 Rejected promise ${position} in ${currentDelay}ms`
             );
           } else {
             Notiflix.Notify.failure(
-              `&#10060 Rejected promise ${position} in ${
-                delayInp + step * (position - 1)
-              }ms`
+              `&#10060 Rejected promise ${position} in ${currentDelay}ms`
             );
           }
         });
-      console.log(totalDelay);
-      clearInterval(timerId);
-    }, Number(step * position));
+
+      console.log(currentDelay);
+    }, currentDelay);
   }
+
   evt.currentTarget.elements.delay.value = '';
   evt.currentTarget.elements.amount.value = '';
   evt.currentTarget.elements.step.value = '';
 }
+
 form.addEventListener('submit', onSubmit);
