cedd562b171beb2c36bbcc0528dc4e3ff6d374ac
olehpetrovasyliev <olegvasiilyevv@gmail.com>
2023-06-01 13:57:29 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -4,15 +4,15 @@ const form = document.querySelector('form');
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
-    setTimeout(() => {
-      if (shouldResolve) {
-        // Fulfill
-        resolve({ position, delay });
-      } else {
-        // Reject
-        reject({ position, delay });
-      }
-    }, delay);
+    // setTimeout(() => {
+    if (shouldResolve) {
+      // Fulfill
+      resolve({ position, delay });
+    } else {
+      // Reject
+      reject({ position, delay });
+    }
+    // }, delay);
   });
 }
 
